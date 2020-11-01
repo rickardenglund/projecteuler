8c38bbd2c1333add26e11125378b69da032472dc
Rickard Englund <rickard.englund@gmail.com>
2020-11-01 19:43:46 +0100

    add timing

--- 25/fib.fsx
+++ 25/fib.fsx
@@ -8,5 +8,8 @@ let rec find i a b =
     if len(b) >= stoplength 
     then i 
     else find (i + 1) b (a + b)
- 
-printf "first with fib number with length %d has index %A\n" stoplength (find 2 1I 1I)
+
+let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+printf "first with fiba number with length %d has index %A\n" stoplength (find 2 1I 1I)
+stopwatch.Stop()
+printf "time: %fms\n" stopwatch.Elapsed.TotalMilliseconds
